6fea1c1d6ab73647c611cc668932af284e4f6760
TheCanadians <vschnoor@mc-kiel.de>
2020-04-07 17:02:59 +0200

    Made UI more user-friendly, improved localization algorithm, added Log File Writer for easier data capture Made CSV File with differences of MS HRTF and Steam Spatializer

--- Audio_Spatial_Recognition/Assets/Scripts/LocalizeAudioSource.cs
+++ Audio_Spatial_Recognition/Assets/Scripts/LocalizeAudioSource.cs
@@ -1,35 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class LocalizeAudioSource : MonoBehaviour
 {
-    private float DbValueR;
-    private float DbValueL;
+    // Db Values of the sound the left and right channel are hearing
+    private float DbValueR, DbValueL;
 
+    [Tooltip("Number of Samples, must be a power of 2. Higher values get better results but calculate slower.")]
     [SerializeField] private int SampleSize = 128;
-    [SerializeField] private float ReferenceValue = 0.0001f;
-    [SerializeField] private float Threshold = 0.0002f;
+    [Tooltip("Step in degrees the algorithm takes each step to determine the Audio Source angle. Smaller value make better results but take longer.")]
+    [SerializeField] private float stepSize = 1f;
 
-    private float[] _samplesR;
-    private float[] _samplesL;
+    private float[] _samplesR, _samplesL;
 
-    private string direction;
-    private string lastDirection;
-
+    // if set to true stops all Listening and Calculations
     private bool stop = false;
 
+    [Tooltip("Time in seconds to listen to Clip. Ignores if set to 0.")]
+    [SerializeField] private float stopTime = 0f;
+    private float nextStop;
+
+    [Tooltip("AudioSource from which the Clips get played. Neccessary to calculate the real angle.")]
     [SerializeField] private AudioSource source;
+    [Tooltip("Array of Clips to listen to. Listens to every Clip in order if playAllClipsAutomatically is true.")]
+    [SerializeField] private AudioClip[] clips;
+    [Tooltip("Plays all Clips in clips automatically if set to true.")]
+    [SerializeField] private bool playAllClipsAutomatically = false;
+    [Tooltip("Number of repetitions of the same Clip the algorithm has to listen to before changing the Clip.")]
+    [SerializeField] private int numberOfRepetitions = 5;
+
+    private int currentClip = 0, repetition = 1;
+
+    [Tooltip("Directory to save the CSV File into. If empty saves to Assets/Data")]
+    [SerializeField] private string pathToSaveTo;
+    [Tooltip("CSV filename. If empty saves as directionValues.csv")]
+    [SerializeField] private string fileName;
+    private bool firstLine = true;
+
+    [Tooltip("Prints the name of the Audio Clip, the current repetition, the estimated angle and the real angle in the console.")]
+    [SerializeField] private bool debug = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initiate array with a size of SampleSize
         _samplesR = new float[SampleSize];
         _samplesL = new float[SampleSize];
+
+        // Checks if clips are available and plays first clip
+        if (clips.Length != 0)
+        {
+            source.clip = clips[currentClip];
+            source.Play();
+            currentClip++;
+        }
+        else
+        {
+            Debug.Log("No Clips provided");
+            Stop();
+        }
+
+        nextStop = Time.time + stopTime;
+            
     }
 
-    // Update is called once per frame
+    // Calculates each frame which direction is louder to rotate into
     void FixedUpdate()
     {
         if (!stop)
@@ -39,38 +77,44 @@ public class LocalizeAudioSource : MonoBehaviour
             DbValueR = AnalyzeSound(_samplesR);
             DbValueL = AnalyzeSound(_samplesL);
 
-            if (direction != null)
-                lastDirection = direction;
-
             if (DbValueR > DbValueL)
             {
-                direction = "right";
-                transform.Rotate(0f, -1f, 0f);
+                transform.Rotate(0f, -stepSize, 0f);
             }
             else if (DbValueR < DbValueL)
             {
-                direction = "left";
-                transform.Rotate(0f, 1f, 0f);
+                transform.Rotate(0f, stepSize, 0f);
             }
-
-            if (lastDirection != direction && lastDirection != null && direction != null)
+            // if the DbValues are -160 no sound is perceived and the SoundClip has probably ended
+            if ((!source.isPlaying) || (stopTime != 0 && Time.time > nextStop))
             {
                 stop = true;
-                Debug.Log("Estimated Angle: " + transform.rotation.eulerAngles.y);
+
+                float estimatedAngle = transform.rotation.eulerAngles.y;
+                
                 transform.LookAt(source.transform);
-                Debug.Log("Real Angle: " + transform.rotation.eulerAngles.y);
+                float realAngle = transform.rotation.eulerAngles.y;
+
+                if (debug)
+                    Debug.Log(source.clip.name + repetition + "  " + estimatedAngle + "   " + realAngle);
+
+                StartCoroutine(WriteToCSV(estimatedAngle, realAngle));
+
+                if (playAllClipsAutomatically)
+                {
+                    Reset();
+                }
             }
-            
         }
 
     }
-
+    // Fills arrays with the samples from the left and right channel
     void GetSoundData()
     {
         AudioListener.GetOutputData(_samplesR, 0);
         AudioListener.GetOutputData(_samplesL, 1);
     }
-
+    // Recalculates the given array into a Db Value
     float AnalyzeSound(float[] array)
     {
         float RmsValue;
@@ -83,11 +127,98 @@ public class LocalizeAudioSource : MonoBehaviour
         }
 
         RmsValue = Mathf.Sqrt(sum / SampleSize);
-        DbValue = 20 * Mathf.Log10(RmsValue / ReferenceValue);
+        DbValue = 20 * Mathf.Log10(RmsValue / 1f);
 
         if (DbValue < -160)
             DbValue = -160;
 
         return DbValue;
+    }
+    // Reset all variables and rotations. Spawns the AudioSource at a new random position
+    private void Reset()
+    {
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+
+        _samplesR = new float[SampleSize];
+        _samplesL = new float[SampleSize];
+
+        nextStop = Time.time + stopTime;
+
+        source.GetComponent<RandomSpawn>().Spawn();
+        
+        if (currentClip < clips.Length && repetition >= numberOfRepetitions)
+        {
+            LoadClip();
+        }
+        else if (repetition < numberOfRepetitions)
+        {
+            repetition++;
+        }
+        else
+        {
+            Stop();
+        }
+
+        source.Play();
+        stop = false;
+    }
+    // Loads next AudioClip
+    private void LoadClip()
+    {
+        source.clip = clips[currentClip];
+        currentClip++;
+        repetition = 1;
+    }
+    // Write the estimated angle, real angle and angle difference between the two to the directionValues CSV file
+    private IEnumerator WriteToCSV(float estAng, float realAng)
+    {
+        string filePath;
+
+        if (pathToSaveTo != "")
+            filePath = pathToSaveTo;
+        else
+            filePath = Application.dataPath + "/Data/";
+
+        if (fileName != "")
+        {
+            filePath += fileName;
+            if (!fileName.ToUpper().Contains(".CSV"))
+                filePath += ".csv";
+        }  
+        else
+            filePath += "directionValues.csv";
+
+        Debug.Log(filePath);
+        StreamWriter writer = new StreamWriter(filePath, true);
+
+        if (firstLine)
+        {
+            writer.WriteLine("Name of Sound;Estimated Angle;Real Angle;Angle Difference");
+            firstLine = false;
+        }
+
+        float angDif = (estAng - realAng);
+
+        if (angDif > 180)
+            angDif -= 360;
+        else if (angDif < -180)
+            angDif += 360;
+
+        writer.WriteLine(source.clip.name + ";" + estAng + ";" + realAng + ";" + angDif);
+
+        writer.Flush();
+        writer.Close();
+
+        yield return null;
+    }
+    // Stop the Program
+    private void Stop()
+    {
+        Debug.Log("Finished");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
     }
 }
--- Audio_Spatial_Recognition/Assets/Scripts/RandomSpawn.cs
+++ Audio_Spatial_Recognition/Assets/Scripts/RandomSpawn.cs
@@ -5,22 +5,23 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class RandomSpawn : MonoBehaviour
 {
-
+    [Tooltip("AudioListener Object which defines the center point of the spawn circle.")]
     public AudioListener listener;
+    [Tooltip("Radius to spawn the GameObject around the AudioListener.")]
     public float radius = 5f;
+
     private Vector3 center;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Set center variable to the AudioListener position
         center = listener.transform.position;
-        Vector3 pos = RandomCircle(center, radius);
-        Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center - pos);
-        transform.rotation = rot;
-        transform.position = pos;
-        //transform.position = new Vector3(Random.Range(0, 100f), Random.Range(0, 100f), Random.Range(0, 100f));
+        // Set AudioSource position to a random point in a circle around the center.
+        Spawn();
     }
 
+    // Calculates random position on the circle
     Vector3 RandomCircle(Vector3 center, float radius)
     {
         float angle = Random.value * 360;
@@ -29,5 +30,14 @@ public class RandomSpawn : MonoBehaviour
         pos.z = center.z + radius * Mathf.Cos(angle * Mathf.Deg2Rad);
         pos.y = center.y;
         return pos;
+    }
+
+    // Spawns the GameObject in a random position in a circle around the AudioListener GameObject and rotates the GameObject to look at the Listener
+    public void Spawn()
+    {
+        Vector3 pos = RandomCircle(center, radius);
+        Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center - pos);
+        transform.rotation = rot;
+        transform.position = pos;
     }
 }
